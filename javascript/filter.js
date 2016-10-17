87ed8fb3819de9a68b9558e225a48608f9e40a66
andiandiandi <kaffeeflow40@web.de>
2016-10-17 20:08:18 +0200

    working filter

--- javascript/filter.js
+++ javascript/filter.js
@@ -1,11 +1,11 @@
 var filter = function(){
-	//var keyword = document.getElementById("searchbar").value;
-	var keyword = "asdf"
+	var keyword = document.getElementById("searchbar").value;
 	var posts = document.getElementsByClassName("blog_post");
 	for(post in posts)
-		if(posts[post].tagName=="DIV" && posts[post] != undefined)
-			if(posts[post].firstElementChild.children[0].innerText.includes(keyword))
-				posts[post].style.visibility = "visible";
+		if(posts[post].children !== undefined)
+			if(posts[post].children[0].text.toLowerCase().indexOf(keyword.toLowerCase()) === -1)
+				 posts[post].style.display = 'none';
 			else
-				posts[post].style.visibility = "hide";
+				posts[post].style.display = 'block';
+
 };
